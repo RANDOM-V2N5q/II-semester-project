fc4e8bc86271e7676aa7a68553ae39f52321cff5
RANDOM-V2N5q <44544007+RANDOM-V2N5q@users.noreply.github.com>
2019-06-15 18:32:36 +0200

    Update MainHeader.h file

--- II-semester-project/MainHeader.h
+++ II-semester-project/MainHeader.h
@@ -1,6 +1,12 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+#include <string>
+#include <vector>
+#include <cmath>
 
 #include "Vector2D.h"
 #include "CircleObject.h"
+#include "Simulation.h"
